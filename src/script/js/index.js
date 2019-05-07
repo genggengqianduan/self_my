018d3c70e14395b5341bcc76c3ddd213925c280f
genggengqian <50280797+genggengqianduan@users.noreply.github.com>
2019-05-07 08:24:03 +0800

    Add files via upload
    首页详情页

---
+++ src/script/js/index.js
@@ -0,0 +1,2 @@
+$('#header').load('public_model/header.html');
+$('#footer').load('public_model/footer.html')
---
+++ src/script/js/registor.js
@@ -0,0 +1,5 @@
+(function(){
+    $('.btn').on('click',function(){
+        $('.registor').css('display','none')
+    })
+})()
